785cd5275018e25d9a2704e5b287ac68339dd211
MohamedHamdy210 <mohamed.hamdyy210@gmail.com>
2024-09-08 21:37:54 +0300

    full game

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ function gameBoard(){
         board[i] = [];
         
         for (let j = 0; j < columns; j++) {
-          board[i].push(0);
+          board[i].push('');
         }
       }
     const getBoard = () => board;
     const selectCell=(r,c,player)=>{
 
-        if(board[r][c]!==0) return 0;
+        if(!board[r][c]=="") return 0;
 
         board[r][c]=player.token;
     
@@ -50,13 +50,23 @@ function gameBoard(){
             const a = board[0][0];
             const b = board[1][1];
             const c = board[2][2];
-        
+                       
             if (a != '0' && a === b && b === c) {
                 alert(`${player.name} is the Winner`);
                     location.reload();
 
             }  
+            const x = board[0][2];
+            const y = board[1][1];
+            const z = board[2][0];
+        
+            if (x != '0' && x === y && y === z) {
+                alert(`${player.name} is the Winner`);
+                    location.reload();
+
+            }  
         }
+        
         else if(rounds===0){
             alert(`Draw`);
             location.reload();
@@ -100,21 +110,115 @@ function gameController(
     const playRound = (r,c) => {
         // Drop a token for the current player
         
-        if(!board.selectCell(r,c, getActivePlayer())) {console.log("Choose another Cell");
-             return;}
+        if(!board.selectCell(r,c, getActivePlayer())) {
+            console.log("Choose another Cell");
+             return 0;
+            }
         
         console.log(
             `Selecting ${getActivePlayer().name}'s token into cell  [${r}] [${c}]...`
           );
         switchPlayerTurn();
         printNewRound();
-        
+        return 1 ;
       };
       printNewRound();
       return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        getBoard: board.getBoard
       };
     
 }
-const game = gameController();
+
+function screenController(){
+    const game = gameController();
+    const turnDiv =document.querySelector('.turn')
+    const boardDiv = document.querySelector('.board');
+    let cell;
+    const updateScreen=()=>{
+        turnDiv.textContent="";
+     const activePlayer = game.getActivePlayer();
+       
+        turnDiv.textContent=`${activePlayer.name}'s Turn`
+       const btText=(ap)=>{
+            document.getElementById(`${parseInt(cell)}`).textContent= `${ap.token}`;
+
+        }
+       
+         return {btText} 
+    }
+    function clickHandlerBoard(e) {
+         cell = parseInt(e.target.value);
+         const ap = game.getActivePlayer();
+        console.log(cell);
+        if (!cell) return;
+        switch (cell) {
+            case 1 : 
+           if( game.playRound(0,0))
+            updateScreen().btText(ap);
+            break;
+        case 2 :  
+        if(game.playRound(0,1))
+            updateScreen().btText(ap);
+
+            break;
+        case 3 :  
+             if(game.playRound(0,2))
+             updateScreen().btText(ap);
+
+            
+            break;
+        case 4 :
+            if(game.playRound(1,0))
+            updateScreen().btText(ap);
+
+            break;
+        case 5 :
+            if(game.playRound(1,1))
+            updateScreen().btText(ap);
+
+            
+            break;
+        case 6 :
+            if(game.playRound(1,2))
+            updateScreen().btText(ap);
+
+            break;
+        case 7 :
+            if(game.playRound(2,0))
+            updateScreen().btText(ap);
+
+            break;
+        case 8 :
+            if(game.playRound(2,1))
+            updateScreen().btText(ap);
+
+            break;
+        case 9 :
+            if(game.playRound(2,2))
+            updateScreen().btText(ap);
+
+            break;
+        
+            default:
+                console.log("def");
+                break;
+        }
+        
+        
+            
+        
+        
+        
+        updateScreen();
+      }
+      boardDiv.addEventListener("click", clickHandlerBoard);
+    
+      // Initial render
+      updateScreen();
+    
+      // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+    }
+    
+    screenController();
